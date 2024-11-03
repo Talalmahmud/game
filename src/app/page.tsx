6137e8cc74991166b56b1470f03fc9e4833247d7
Talal Mahmud <mahmudtalal2@gmail.com>
2024-11-03 23:26:03 +0600

    upload project'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,157 @@
-import Image from "next/image";
+"use client";
+import axios from "axios";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [firstPlayer, setFirstPlayer] = useState<string>("");
+  const [secondPlayer, setSecondPlayer] = useState<string>("");
+  const [firstResult, setFirstResult] = useState<
+    { text: string; result: number }[]
+  >([]);
+  const [secondResult, setSecondResult] = useState<
+    { text: string; result: number }[]
+  >([]);
+  const [firstPlayerDisable, setFirstPlayerDisable] = useState<boolean>(false);
+  const [secondPlayerDisable, setSecondPlayerDisable] = useState<boolean>(true);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+  const textLengthCheck = (testText: string) => {
+    return testText?.length >= 4 ? true : false;
+  };
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const dictionarySearch = async (searchText: string) => {
+    try {
+      const res = await axios.get(
+        "https://api.dictionaryapi.dev/api/v2/entries/en/" + searchText
+      );
+      const resData = res.data;
+
+      return {
+        text: searchText,
+        result: searchText.length > 4 ? 5 + (searchText.length - 5) : 5,
+      };
+    } catch (error: any) {
+      setErrorMessage(error?.response?.data?.title);
+      return { text: searchText, result: -1 };
+    }
+  };
+  const handleFirstKey = async (e: any) => {
+    if (e.key === "Enter") {
+      if (textLengthCheck(firstPlayer)) {
+        setFirstPlayerDisable(true);
+        setSecondPlayerDisable(false);
+        const responseData = await dictionarySearch(firstPlayer);
+        console.log(responseData);
+        setFirstResult([...firstResult, responseData]);
+
+        setFirstPlayer("");
+      } else {
+        setErrorMessage("Word is less than 4");
+      }
+    }
+  };
+  const handleSecondKey = async (e: any) => {
+    if (e.key === "Enter") {
+      if (textLengthCheck(secondPlayer)) {
+        setSecondPlayerDisable(true);
+        setFirstPlayerDisable(false);
+        const responseData = await dictionarySearch(secondPlayer);
+        setSecondResult([...secondResult, responseData]);
+        setSecondPlayer("");
+      } else {
+        setErrorMessage("Word is less than 4");
+      }
+    }
+  };
+
+  const handleFirstFocus = async (e: any) => {
+    if (textLengthCheck(firstPlayer)) {
+      setFirstPlayerDisable(true);
+      setSecondPlayerDisable(false);
+      const responseData = await dictionarySearch(firstPlayer);
+      setFirstResult([...firstResult, responseData]);
+      setFirstPlayer("");
+    } else {
+      setErrorMessage("Word is less than 4");
+    }
+  };
+  const handleSecondFocus = async (e: any) => {
+    if (textLengthCheck(secondPlayer)) {
+      setSecondPlayerDisable(true);
+      setFirstPlayerDisable(false);
+      const responseData = await dictionarySearch(secondPlayer);
+
+      setSecondResult([...secondResult, responseData]);
+      setSecondPlayer("");
+    } else {
+      setErrorMessage("Word is less than 4");
+    }
+  };
+  return (
+    <div className=" bg-gray-800 w-full min-h-screen  ">
+      <div className=" w-full h-full flex items-center justify-center gap-6 pt-16">
+        <div className=" flex  flex-col gap-2 w-[400px]">
+          <p className=" text-[20px] font-semibold text-white">Player 1</p>
+          <input
+            type="text"
+            value={firstPlayer}
+            disabled={firstPlayerDisable}
+            onChange={(e: any) => {
+              setFirstPlayer(e.target.value);
+              if (textLengthCheck(e.target.value)) {
+                setSecondPlayerDisable(false);
+              }
+            }}
+            onKeyDown={handleFirstKey}
+            onFocus={handleSecondFocus}
+            className={` ${
+              firstPlayerDisable ? "bg-gray-600" : "bg-gray-400"
+            } text-black w-full p-1 outline-none text-[16px] rounded-md`}
+          />
+          <p className=" text-red-800">{errorMessage}</p>
+          <div className=" flex flex-col gap-2 h-[400px] overflow-auto">
+            {firstResult?.map((item: any, index: number) => (
+              <div
+                key={index}
+                className=" flex justify-between w-full text-white"
+              >
+                <p>{item?.text}</p>
+                <p>{item?.result}</p>
+              </div>
+            ))}
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+        <div className=" flex  flex-col gap-2 w-[400px]">
+          <p className=" text-[20px] font-semibold text-white">Player 2</p>
+          <input
+            type="text"
+            value={secondPlayer}
+            disabled={secondPlayerDisable}
+            onChange={(e: any) => {
+              setSecondPlayer(e.target.value);
+              if (textLengthCheck(e.target.value)) {
+                setFirstPlayerDisable(false);
+              }
+            }}
+            onKeyDown={handleSecondKey}
+            onFocus={handleFirstFocus}
+            className={` ${
+              secondPlayerDisable ? "bg-gray-600" : "bg-gray-400"
+            } text-black w-full p-1 outline-none text-[16px] rounded-md`}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <p className=" text-red-800">{errorMessage}</p>
+          <div className=" flex flex-col gap-2 h-[400px] overflow-auto">
+            {secondResult?.map((item: any, index: number) => (
+              <div
+                key={index}
+                className=" flex justify-between w-full text-white"
+              >
+                <p>{item?.text}</p>
+                <p>{item?.result}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
